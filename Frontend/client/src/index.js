ba62cb7c45a83e7d4636c437935483b6bcb0e553
Freekeyboy <jjhutton81@gmail.com>
2019-02-28 20:46:02 -0800

    Set front end dependencies. Added CSS reset tool. Set up index.js for added dependencies.

--- Frontend/client/src/index.js
+++ Frontend/client/src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import thunk from "redux-thunk"
+import logger from 'redux-logger'
+import rootReducer from "./reducers"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Middleware for error logging and dispatching
+const middleware = applyMiddleware(logger, thunk);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(rootReducer, middleware);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
