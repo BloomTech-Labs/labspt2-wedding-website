6c865aa8efac500bbb87853125b1bfbfaf2fd2e4
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-03-02 12:46:04 -0600

    Front-end Development Updates: > src/index.js: took out rootReducer and replaced with vanilla reducer in store

--- Frontend/client/src/index.js
+++ Frontend/client/src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Provider } from "react-redux";
 import App from "./App";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import thunk from "redux-thunk"
-import logger from 'redux-logger'
-import rootReducer from "./reducers"
+//import logger from 'redux-logger'
+import reducer from "./reducer"
+import { Provider } from "react-redux";
 
 //Middleware for error logging and dispatching
-const middleware = applyMiddleware(logger, thunk);
+const middleware = applyMiddleware(thunk);
+const store = createStore(reducer, middleware);
 
-const store = createStore(rootReducer, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
