fecb84a1e1616e923a4c47659e7aa86f5d4bcc30
Chris Roth <c.roth.lambda@gmail.com>
2019-03-02 14:43:06 -0600

    Development back-end updates: added tables, userPhoto, livePhoto, guestList and guest Address. checked migration and rolled back incase changes need to be made.

--- Backend/data/migrations/20190302134856_userPhotos_table.js
+++ Backend/data/migrations/20190302134856_userPhotos_table.js
@@ -1,10 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('userPhotos', tbl=>{
-      
+  return knex.schema.createTable('usersPhotos', tbl=>{
+      tbl.increments(),
+      tbl.string('imgUrl').notNullable(),
+      tbl.string('userId').unsigned()
+      tbl.foreign('userId').references('users.id')
   })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('userPhotos')
 };
---
+++ Backend/data/migrations/20190302142436_guestAddress_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('guestAddress', tbl=>{
+      tbl.increments(),
+      tbl.string('guestId').unsigned(),
+      tbl.foreign('guestId').references('guestList.id'),
+      tbl.string('address').notNullable(),
+      tbl.string('zipCode').notNullable(),
+      tbl.string('city').notNullable(),
+      tbl.string('state').notNullable(),
+      tbl.string('country').notNullable()
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('guestAddress')
+};
