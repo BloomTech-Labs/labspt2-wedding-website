284164190f1ab8e30620af1c5dcb5ca207b048dd
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-03-05 21:54:57 -0600

    beRoutes: added guest by id endpoint commented guest list table with rsvp stuff

--- Backend/Config/routes/userRoute.js
+++ Backend/Config/routes/userRoute.js
@@ -1,23 +1,23 @@
-const helper= require('../helpers/userDb');
+const helper = require('../helpers/userDb')
 
-module.exports = server =>{
-    server.get('/users', allUsers),
-    server.post('/register', register)
+module.exports = server => {
+  server.get('/users', allUsers), server.post('/register', register)
 }
-allUsers = (req, res)=>{
-    helper.getUsers().then(users => {
-      res.status(201).json({message:'success'})
-    })
-}
-
-register = (req, res)=> {
-  const creds = req.body;
-  //Need to add: first/last name and email required if statement
-  helper.addUser(creds).then(newUser => {
-    res.status(201).json(newUser)
-  }).catch((err) => {
-    res.status(500).json({message: 'Failed to add user'})
+allUsers = (req, res) => {
+  helper.getUsers().then(users => {
+    res.status(201).json({ message: 'success' })
   })
+}
 
-
+register = (req, res) => {
+  const creds = req.body
+  //Need to add: first/last name and email required if statement
+  helper
+    .addUser(creds)
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to add user' })
+    })
 }
