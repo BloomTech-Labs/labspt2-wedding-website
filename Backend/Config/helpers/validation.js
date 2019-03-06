9c0ae4753e72127595ee4d2158840d3ecc6a09fa
Chris Roth <c.roth.lambda@gmail.com>
2019-03-05 22:08:44 -0600

    beRoutes formatting: formatted the validation file to resolve conflict

--- Backend/Config/helpers/validation.js
+++ Backend/Config/helpers/validation.js
@@ -1,9 +1,11 @@
 const Joi = require('joi');
 
-module.exports={
-    userInput:{
+module.exports = {
+    userInput: {
         username: Joi.string().required(),
         password: Joi.string().min(6).required(),
-        email: Joi.string().email({minDomainAtoms:2})
+        email: Joi.string().email({
+            minDomainAtoms: 2
+        })
     }
 }
