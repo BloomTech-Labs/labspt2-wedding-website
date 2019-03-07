597f06367fd909a00e35112ed893ad9368263dc1
Chris Roth <c.roth.lambda@gmail.com>
2019-03-06 23:54:10 -0600

    Deployment: updates to scripts and knex trying to resolve deployment errors

--- Backend/knexfile.js
+++ Backend/knexfile.js
@@ -1,11 +1,4 @@
 require('dotenv').config();
-const localPg={
-  host: 'localhost',
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS
-}
-const dbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
 
@@ -41,7 +34,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: dbConnection,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
