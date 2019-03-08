f9028af9c42b0a674e63cf6fc83e27e2b522c12a
Joe Mercado <jryel1970@gmail.com>
2019-03-07 22:40:14 -0500

    pushing up rsvp and pricing page

--- Frontend/client/src/components/pages/rsvp.js
+++ Frontend/client/src/components/pages/rsvp.js
@@ -16,10 +16,26 @@ export default class RSVP extends Component {
         <label for="rsvpForm">Will you be attending our wedding?</label>
             <div className="rsvpArea">
                 <input type="radio" name="attending" className="radio" /><label for="rsvpArea">Attending</label>
-                <input typ="radio" name="non-attending" className="radio" /><label for="rsvpArea">Not Attending</label>
+                <input type="radio" name="non-attending" className="radio" /><label for="rsvpArea">Not Attending</label>
             </div>
             <p className="fine-print">Ask each individual guest</p>
         </div>
+        <div className="rsvpForm">
+            <label for="rsvpForm">What is your mailing address?</label>
+            <input type="text" name="address" placeholder="address" className="textBox" />
+            <p className="fine-print">Ask once per household.</p>
+        </div>
+        <div className="rsvpForm">
+            <label for="rsvpForm">Are you friend or family of...?</label>
+            <div className="rsvpArea">
+                <input type="radio" name="Bride" className="radio" /><label for="Bride">Bride Name</label>
+                <input type="radio" name="Groom" className="radio" /><label for="Grrom">Groom Name</label>
+                <input type="radio" name="Both" className="radio" /><label for="Both">Both</label>
+                <p className="fine-print">Ask each individual guest</p>
+            </div>
+        </div>
+        <div className="button">Add Question</div>
+        <div className="button">Save</div>
     </div>
       )
     };
