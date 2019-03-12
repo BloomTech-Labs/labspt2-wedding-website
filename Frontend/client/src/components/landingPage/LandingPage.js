49b5fac0837c26d97d830cf9ebfbc6438b0f1e9a
Freekeyboy <jjhutton81@gmail.com>
2019-03-11 20:33:40 -0700

    Added styled components to landingpage branch. Added login/signup button and linked it to the login component.

--- Frontend/client/src/components/landingPage/LandingPage.js
+++ Frontend/client/src/components/landingPage/LandingPage.js
@@ -1,28 +1,44 @@
 import React, { Component } from "react";
-import Login from './login';
 import DemoCarousel from "./Carousel";
+import { Route, Link } from "react-router-dom";
+import Login from "./login";
+
+import styled from "styled-components";
+
+const LogInButton = styled.button`
+position: absolute;
+z-index: 1;
+margin: 3%;
+`
 
 export default class LandingPage extends Component {
-  render(){
+  render() {
     return (
+      <div>
         <div>
-            <DemoCarousel />
-            <br />
-            <div style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              color: 'black'
-            }}>
-            </div>
-            <br />
-            <div style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              color: 'black'
-            }}>
-            </div>
+          <LogInButton>
+            <Link to="/login">Log In or Sign Up</Link>
+          </LogInButton>
+          <Route exact path="/login" component={Login} />
         </div>
-    )
+        <DemoCarousel />
+        <br />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            color: "black"
+          }}
+        />
+        <br />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            color: "black"
+          }}
+        />
+      </div>
+    );
   }
-};  
-
+}
