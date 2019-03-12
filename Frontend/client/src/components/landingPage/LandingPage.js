053dcf6e2f161ac80d1e313da920400a5088afd8
Freekeyboy <jjhutton81@gmail.com>
2019-03-11 19:40:39 -0700

    Deleted unneeded components from landingPage.js. Fixed root folder structure for pricing.js.

--- Frontend/client/src/components/landingPage/LandingPage.js
+++ Frontend/client/src/components/landingPage/LandingPage.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import Login from './login';
 import DemoCarousel from "./Carousel";
-import SideNav from '../sidenav';
-import Pricing from '../pages/pricing';
-import RSVP from '../pages/rsvp';
 
 export default class LandingPage extends Component {
   render(){
@@ -17,8 +14,6 @@ export default class LandingPage extends Component {
               justifyContent: 'space-between',
               color: 'black'
             }}>
-              <SideNav />
-              <Pricing />
               <div><a href="#">Logout</a></div>
             </div>
             <br />
@@ -27,8 +22,6 @@ export default class LandingPage extends Component {
               justifyContent: 'space-between',
               color: 'black'
             }}>
-              <SideNav />
-              <RSVP />
               <div><a href="#">Logout</a></div>
             </div>
             
