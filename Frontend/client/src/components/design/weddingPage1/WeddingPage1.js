2d8b039591148b102abdd0c4b15494c2df0a7371
Freekeyboy <jjhutton81@gmail.com>
2019-03-12 21:02:37 -0700

    Added React-native for imagebackground function. Added babel-core for peer dependancy. App won't recognize react-native import. Will troubleshoot more tomorrow.

---
+++ Frontend/client/src/components/design/weddingPage1/WeddingPage1.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import { ImageBackground } from "react-native";
+
+export default class WeddingPage1 extends Component {
+  render() {
+    return (
+    
+          <ImageBackground
+            source={"https://i.ibb.co/cvfBVh0/Scrap-Book-Background.jpg"}
+            style={{ width: "100%", height: "100%" }}
+          />
+      
+    );
+  }
+}
