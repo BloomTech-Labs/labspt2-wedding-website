b757ecf1ca5c346e41251acfb1dc938e8e54dd82
Chris Roth <c.roth.lambda@gmail.com>
2019-03-13 23:03:55 -0500

    beAWSbucket: setting up boiler plate.

--- Backend/Api/server.js
+++ Backend/Api/server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const logger = require('morgan')
-const server = express()
+
 const configGuestRoutes = require('../Config/routes/guestRoute')
 const configUserRoutes = require('../Config/routes/userRoute')
 const configRsvpRoutes = require('../Config/routes/rsvpRoute')
+
+const server = express()
 
 server.use(express.json(), logger('dev'), cors(), helmet())
 configUserRoutes(server)
