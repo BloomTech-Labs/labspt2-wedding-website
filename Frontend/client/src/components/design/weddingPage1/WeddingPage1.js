5595e41cfda2561daf3974e52a2c3b16ecfdf479
Freekeyboy <jjhutton81@gmail.com>
2019-03-13 18:34:44 -0700

    Removed react-native. It will not work for this project.

--- Frontend/client/src/components/design/weddingPage1/WeddingPage1.js
+++ Frontend/client/src/components/design/weddingPage1/WeddingPage1.js
@@ -1,16 +1,7 @@
 import React, { Component } from "react";
 
-import { ImageBackground } from "react-native";
-
 export default class WeddingPage1 extends Component {
   render() {
-    return (
-    
-          <ImageBackground
-            source={"https://i.ibb.co/cvfBVh0/Scrap-Book-Background.jpg"}
-            style={{ width: "100%", height: "100%" }}
-          />
-      
-    );
+    return <div />;
   }
 }
