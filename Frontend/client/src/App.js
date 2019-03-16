d274ebd4ffc7dd0c003b7c90a6beb03cc106ecc0
Freekeyboy <jjhutton81@gmail.com>
2019-03-16 11:44:37 -0700

    Created file WeddingPage3.js, and corresponding branch. Styled page, and added route. Three new fontsw added on index.html.

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -5,7 +5,7 @@ import LandingPage from "./components/landingPage/LandingPage";
 import Login from "./components/landingPage/Login";
 import WeddingPage1 from "./components/design/weddingPage1/WeddingPage1";
 import WeddingPage2 from "./components/design/weddingPage2/WeddingPage2";
-
+import WeddingPage3 from "./components/design/weddingPage3/WeddingPage3";
 import './App.css';
 
 class App extends Component {
@@ -16,6 +16,7 @@ class App extends Component {
         <Route exact path="/login" component={Login} />
         <Route exact path="/design1" component={WeddingPage1} />
         <Route exact path="/design2" component={WeddingPage2} />
+        <Route exact path="/design3" component={WeddingPage3} />
       </div>
     );
   }
