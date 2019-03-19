d241c8f346e49bc2bc581451a34bbe4fff2325ac
Joe Mercado <jryel1970@gmail.com>
2019-03-18 20:13:00 -0400

    pushing up to FEroutes

--- Frontend/client/src/components/pricing/Pricing.js
+++ Frontend/client/src/components/pricing/Pricing.js
@@ -1,23 +1,51 @@
-import React, { Component } from 'react';
-import './pricing.css'
+import React, { Component } from 'react'
+
+const pricing = {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '0 auto'
+
+}
+
+const box = {
+    display: 'flex',
+    flexDirection: 'row',
+    border: '2px solid #000000',
+    width: '100%',
+    height: '50%'
+}
+
+const boxLi = {
+    listStyleType: 'none',
+    margin: '5px 0'
+}
+
+const pricingLogo = {
+    textAlign: 'center'
+}
+
+const boxList = {
+    display: 'flex',
+    flexDirection: 'column',
+}
 
 export default class Pricing extends Component {
     render() {
         return (
-            <div className="pricing">
-            <h1 className="price-logo">Pricing</h1>
-            <hr />
-            <div className="box">
-                <ul className="list-box">
-                    <li>Planning Dashboard</li>
-                    <li>Digital RSVPs</li>
-                    <li>Free Website Hosting</li>
-                    <li>Wedding Website</li>
-                </ul>
-            </div>
-            <hr />
-            <h2>$0</h2>
-         </div>
+             <div style={pricing}>
+                 <h1 style={pricingLogo}>Pricing</h1>
+                 <hr />
+                 <div style={box}>
+                    <ul style={boxList}>
+                        <li style={boxLi}>Planning Dashboard</li>
+                        <li style={boxLi}>Digital RSVPs</li>
+                        <li style={boxLi}>Free Website Hosting</li>
+                        <li style={boxLi}>Wedding Website</li>
+                    </ul>
+                 </div>
+                 <hr />
+                 <h2>$0</h2>
+             </div>
         );
     }
 }
--- Frontend/client/src/components/sidenav/sidenav.js
+++ Frontend/client/src/components/sidenav/sidenav.js
@@ -1,25 +1,95 @@
-import React, { Component } from 'react';
-import '../components/sidenav.css';
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Pricing from '../pricing/pricing'
+import RSVP from '../rsvp/rsvp'
+import Billing from '../pages/billing'
+import Settings from '../pages/settings'
+// import DemoCarousel from '../components/Carousel'
+
+
+const sideNav = {
+    display: 'flex',
+    flexDirection: 'row',
+    height: '100%',
+    justifyContent: 'left',
+    width: '20%'
+}
+
+const menuLogo = {
+    display: 'flex',
+    width: '92px',
+    backgroundSize: 'cover 100%',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    margin: '0',
+    textDecoration: 'none',
+    textTransform: 'uppercase'
+}
+
+const menu = {
+    backgroundColor: '#FFFFFF',
+    boxShadow: '0px 2px 24px 0px rgba(0, 0, 0, 0.15)',
+    borderRadius: '8px',
+    display: 'flex',
+    padding: '0 40px',
+    position: 'relative'
+}
+
+const menuListItem = {
+    listStyleType: 'none',
+    margin: '10px -30px'
+}
+
+
+const menuList = {
+    listStyleType: 'none',
+    padding: '0',
+    height: '100%',
+    margin: '0',
+    marginRight: '40px',
+}
+
+const menuLink = {
+    color: '#3A3E47',
+    display: 'inline-block',
+    height: '100%',
+    fontSize: '16px',
+    textDecoration: 'none',
+    textTransform: 'uppercase',
+    alignItems: 'center',
+    padding: '0 3px'
+}
+
+
 
 export default class Navigation extends Component {
     render() {
         return (
-            <Router>
-                <div className="sideNav">
-                    <h3 className="menu-logo"><a href="https://">Home</a> > Pricing</h3>
-                    <nav className="menu">
-                        <div className="menu_right">
-                            <ul className="menu_list">
-                                <li className="menu_list-item"><a href="http://" className="menu_link menu_link-active">Home</a></li>
-                                <li className="menu_list-item"><a href="http://" className="menu_link">Designs</a></li>
-                                <li className="menu_list-item"><a href="http://" className="menu_link">Pricing</a></li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
-            </Router>
-             
+             <Router>
+                 <div>
+                     <h3><Link style={menuLogo} to="/">Home</Link></h3>
+                    <div style={sideNav}>
+                        <nav style={menu}>
+                            <div>
+                                <ul style={menuList}>
+                                    <li style={menuListItem}><Link to="/" style={menuLink}>Home</Link></li>
+                                    <li style={menuListItem}><Link to="/settings" style={menuLink}>Settings</Link></li>
+                                    <li style={menuListItem}><Link to="/pricing" style={menuLink}>Pricing</Link></li>
+                                    <li style={menuListItem}><Link to="/billing" style={menuLink}>Billing</Link></li>
+                                    <li style={menuListItem}><Link to="/rsvp" style={menuLink}>RSVP</Link></li>
+                                </ul>
+                            </div>
+                        </nav>
+                        <Route exact path="/"></Route>
+                        <Route exact path="/settings" component={Settings}></Route>
+                        <Route exact path="/pricing" component={Pricing}></Route>
+                        <Route exact path="/billing" component={Billing}></Route>
+                        <Route exact path="/rsvp" component={RSVP}></Route>
+                        
+                    </div>
+                    <div></div>
+                 </div>
+             </Router>
         );
     }
 }
