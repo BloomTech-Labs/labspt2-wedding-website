8de9762114f783fbabfbe93ee7eeda3c09f443d7
Chris Roth <c.roth.lambda@gmail.com>
2019-03-18 21:21:14 -0500

    beAuth: updates to the package.json - added up carrot for node and yarn engines

--- Backend/Config/dbConfig.js
+++ Backend/Config/dbConfig.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const db = process.env.TESTING_DB || process.env.DB|| 'development';
+const db = process.env.TESTING_DB || 'development'|| process.env.DB;
 
 const config = require('../knexfile')[db];
 
