5959537541726b5f4595686ce3c44d3a0bddd736
Chris Roth <c.roth.lambda@gmail.com>
2019-03-18 20:35:45 -0500

    beAWSbucket: updates to the upload function

--- Backend/Config/routes/photoUploadRoute.js
+++ Backend/Config/routes/photoUploadRoute.js
@@ -4,15 +4,15 @@ const singleUpload = upload.single('image')
 
 module.exports = server => {
     server.post('/users/:id/upload', addImage)
-    server.get('/:id/live-photos', GetLivePhotos)
+    server.get('/users/:id/live-photos', GetLivePhotos)
 }
 
 addImage = (req, res) => {
-    const userId= req.params.id;
-    singleUpload(req, res, err =>{
-        if(err){
+    const userId = req.params.id;
+    singleUpload(req, res, err => {
+        if (err) {
             res.status(422).send({
-                errors:[{
+                errors: [{
                     title: 'Image Upload Error',
                     detail: err.message
                 }]
@@ -20,23 +20,28 @@ addImage = (req, res) => {
         }
         const image = req.file.location;
         console.log(image)
-        const newImage={
+        const newImage = {
             imgUrl: image,
             name: 'test',
             user_id: userId
         }
-        db('livePhoto').insert(newImage).then(id=>{
+        console.log(newImage)
+        db('livePhoto').insert(newImage).then(id => {
             res.status(201).send(newImage)
-        }).catch(err=>{
-            res.status(500).send({error: err})
+        }).catch(err => {
+            res.status(500).send({
+                error: err
+            })
         })
     })
 }
 
-GetLivePhotos = (req, res)=>{
-    const { id }=req.params
-    db('livePhoto').select(id, 'user_id').then(tbl=>
-        res.json(tbl)).catch(err=>{
-            res.status(500).send(err)
-        })
+GetLivePhotos = (req, res) => {
+    const {
+        id
+    } = req.params
+    db('livePhoto').select('user_id', id).then(tbl =>
+        res.json(tbl)).catch(err => {
+        res.status(500).send(err)
+    })
 }
