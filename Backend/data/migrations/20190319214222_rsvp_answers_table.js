6aefc1e7d67022913f61a902c2e4503732ed260e
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-03-19 21:43:07 -0500

    Backen: added WES tables for rsvp questions and answers

---
+++ Backend/data/migrations/20190319214222_rsvp_answers_table.js
@@ -0,0 +1,25 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('rsvpAnswers', table => {
+    table.increments()
+    table.string('answer_body')
+    table.integer('rsvpQuestions_id').unsigned()
+    table
+      .foreign('rsvpQuestions_id')
+      .references('id')
+      .on('users')
+    table.integer('users_id').unsigned()
+    table
+      .foreign('users_id')
+      .references('id')
+      .on('users')
+    table.integer('guestList_id').unsigned()
+    table
+      .foreign('guestList_id')
+      .references('id')
+      .on('guestList')
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('rsvpAnswers')
+}
