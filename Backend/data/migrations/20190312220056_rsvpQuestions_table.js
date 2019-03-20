8870b5aa5f4ca2f2adda644e038b4ebc1e6e11e3
Wesley Meeks <wesmeeks88@gmail.com>
2019-03-19 20:21:13 -0600

    Created rsvp questions and rsvp answers tables.

---
+++ Backend/data/migrations/20190312220056_rsvpQuestions_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('rsvpQuestions', table => {
+      table.increments();
+      table.string('Question_body').notNullable();
+      table.integer('users_id').unsigned();
+      table.foreign('users_id').references('id').on('users');
+      table.integer('guestList_id').unsigned();
+      table.foreign('guestList_id').references('id').on('guestList')
+   
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('rsvpQuestions');
+};
