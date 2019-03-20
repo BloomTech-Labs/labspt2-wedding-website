1f6600c2f734ff09dec1b76dfdcc058daa8f8ab1
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-03-19 22:10:56 -0500

    dbSeeds: created seed files + users seed done

---
+++ Backend/data/seeds/01_users.js
@@ -0,0 +1,46 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('table_name')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('table_name').insert([
+        // basic User
+        {
+          id: 2,
+          email: 'yup@gmail.com',
+          username: 'Boi1',
+          password: 'alkjse',
+          boolean: false,
+        },
+        {
+          id: 1,
+          email: 'bla@gmail.com',
+          username: 'Boi2',
+          password: 'dkshfbv',
+          boolean: false,
+        },
+        {
+          id: 3,
+          email: 'meh@gmail.com',
+          username: 'Boi3',
+          password: 'slejnr',
+          boolean: true,
+        },
+        {
+          id: 4,
+          email: 'sup@gmail.com',
+          username: 'Boi4',
+          password: 'sdjfo',
+          boolean: false,
+        },
+        {
+          id: 5,
+          email: 'blu@gmail.com',
+          username: 'Boi5',
+          password: 'msnzdl',
+          boolean: true,
+        },
+      ])
+    })
+}
