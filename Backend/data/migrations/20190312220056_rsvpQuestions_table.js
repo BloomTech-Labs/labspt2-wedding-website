a162cb2ec3584d82c74ab677eeae36023b22fecf
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-03-20 22:51:05 -0500

    Backend: seeded dta to tables

--- Backend/data/migrations/20190312220056_rsvpQuestions_table.js
+++ Backend/data/migrations/20190312220056_rsvpQuestions_table.js
@@ -1,17 +1,20 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('rsvpQuestions', table => {
-      table.increments();
-      table.string('Question_body').notNullable();
-      table.integer('users_id').unsigned();
-      table.foreign('users_id').references('id').on('users');
-      table.integer('guestList_id').unsigned();
-      table.foreign('guestList_id').references('id').on('guestList')
-   
-
+    table.increments()
+    table.string('Question_body').notNullable()
+    table.integer('users_id').unsigned()
+    table
+      .foreign('users_id')
+      .references('id')
+      .on('users')
+    table.integer('guestList_id').unsigned()
+    table
+      .foreign('guestList_id')
+      .references('id')
+      .on('guestList')
   })
-};
+}
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('rsvpQuestions');
-};
+  return knex.schema.dropTableIfExists('rsvpQuestions')
+}
