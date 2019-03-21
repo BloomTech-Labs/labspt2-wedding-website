caffa5b77e6f58e9d333ac5b2a4483dd3b9b79b3
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-03-20 20:49:29 -0500

    feRedux: added more actions keep working on them

--- Frontend/client/src/actions/index.js
+++ Frontend/client/src/actions/index.js
@@ -6,6 +6,10 @@ export const LOGIN_REGISTER_ERROR = "LOGIN_REGISTER_ERROR"
 export const FETCHING = 'FETCHING'
 export const GET_USERS = 'GET_USERS'
 export const FETCHING_ERROR = 'FETCHING_ERROR'
+export const GET_GUESTS = 'GET_GUESTS'
+export const GET_GUEST = 'GET_GUEST'
+export const UPDATING = 'UPDATING'
+export const 
 
 const
 
@@ -31,5 +35,40 @@ export const loginRegister = creds => dispatch => {
 export const fetchUsers = () => dispatch => {
   dispatch({type: FETCHING})
   axios 
-    .get('https://joinourbigday.herokuapp.com//users')
+    .get('https://joinourbigday.herokuapp.com/users')
+    .then(res => {
+      dispatch({
+        type: GET_USERS,
+        payload: res.data
+      })
+    })
+    .catch(err => {
+      dispatch({
+        type: FETCHING_ERROR
+      })
+    })
+}
+
+export const fetchUser = id => dispatch => {
+  dispatch({type: FETCHING})
+  axios 
+    .get(`https://joinourbigday.herokuapp.com/users/${id}`)
+    .then(res => {
+      dispatch({
+        type: GET_USER,
+        payload: res.data
+      })
+    })
+    .catch(err => {
+      dispatch({
+        type: FETCHING_ERROR
+      })
+    })
+}
+
+export const editUser = (id, user) => dispatch => {
+  dispatch({type: UPDATING})
+  axios 
+    .put(`https://joinourbigday.herokuapp.com/users/${id}`)
+    .then(() => fetch)
 }
