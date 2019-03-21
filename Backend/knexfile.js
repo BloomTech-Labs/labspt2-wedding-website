2c59d7c68f5d733ea4b45c366cf797bbbe0af7dc
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-03-20 22:08:32 -0500

    feRedux: typo fix

--- Backend/knexfile.js
+++ Backend/knexfile.js
@@ -1,65 +1,62 @@
-require('dotenv').config();
-
+require('dotenv').config()
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/labsDB.sqlite3'
-    }, 
+      filename: './data/labsDB.sqlite3',
+    },
     useNullAsDefault: true,
-    migrations:{
-      directory: './data/migrations'
-    }, 
-    seeds:{
-      directory: '.data/seeds'
-    }
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
   },
 
   staging: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
     client: 'pg',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
   testing: {
     client: 'sqlite3',
     connection: {
-      filename: './data/guidrTestDB.sqlite3'
+      filename: './data/guidrTestDB.sqlite3',
     },
     useNullAsDefault: true,
-    migrations:{
-      directory: './data/migrations'
+    migrations: {
+      directory: './data/migrations',
     },
-    seeds:{
-      directory: './data/seeds'
+    seeds: {
+      directory: './data/seeds',
     },
-  }
-
-};
+  },
+}
