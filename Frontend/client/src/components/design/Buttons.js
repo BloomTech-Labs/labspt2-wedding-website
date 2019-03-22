2e020ad29356c0f1beb2256ba7aa7ea7d29d4a45
Freekeyboy <jjhutton81@gmail.com>
2019-03-21 20:09:48 -0700

    Removed redundant import.

--- Frontend/client/src/components/design/Buttons.js
+++ Frontend/client/src/components/design/Buttons.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faImages, faImage } from '@fortawesome/free-solid-svg-icons'
+import { faImage } from '@fortawesome/free-solid-svg-icons'
 
 export default props => 
   <div className='buttons fadein'>
