3a4dd8491a16405fd9c99298b7dc67f71bb30e62
FreeKeyBoy <jjhutton81@gmail.com>
2019-04-01 20:05:30 -0700

    created table for user site

--- Backend/data/migrations/20190401215501_userCustomPage_table.js
+++ Backend/data/migrations/20190401215501_userCustomPage_table.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('userSite'), tbl => {
+    tbl.increments()
+    tbl.integer('guestId').unsigned()
+    tbl.foreign('guestId').references('guestList.id')
+    tbl.string('siteImg')
+    tbl.string('coupleNameSite').notNullable()
+    tbl.string('meetingStory')
+    tbl.string('proposal')
+  }
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('userSite')
 };
