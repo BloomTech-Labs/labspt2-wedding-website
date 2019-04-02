4a7e586140a78bd454a66b99ce392e24ec194ced
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-04-01 21:55:50 -0500

    added new table for custom user sites

--- Frontend/client/src/components/rsvp/GuestList.js
+++ Frontend/client/src/components/rsvp/GuestList.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react'
-import {
-    BrowserRouter as Router,
-    Route, Link
-} from 'react-router-dom'
-import RSVP from './RSVP'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import RSVP from './rsvp'
 import { FaPlus } from 'react-icons/fa'
 
 export default class GuestList extends Component {
-    render() {
-      return (
-        <Router>
-            <div>
-                <Link to="/rsvp"><FaPlus />Add Guest Manually</Link>
-                {/* <label htmlFor="FaPlus">Add Guest Manually</label> */}
-                <Route path='/rsvp' component={RSVP}/>
-            </div>
-            
-        </Router>
-      )
-    }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Link to='/rsvp'>
+            <FaPlus />
+            Add Guest Manually
+          </Link>
+          {/* <label htmlFor="FaPlus">Add Guest Manually</label> */}
+          <Route path='/rsvp' component={RSVP} />
+        </div>
+      </Router>
+    )
+  }
 }
