7b8542db9deb21f443a5ff8cedf36ce2938bf1a9
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-04-08 21:13:44 -0500

    fsRedux: new stuuf

---
+++ Frontend/client/src/components/modals/registry.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Registry extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return <div />
+  }
+}
+
+export default Registry
