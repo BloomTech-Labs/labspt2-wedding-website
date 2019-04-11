c74175a2206d928351183b637138d328c38f1903
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-04-11 11:49:55 -0500

    FEregistry: created registry component

--- Frontend/client/src/components/modals/registry.js
+++ Frontend/client/src/components/modals/registry.js
@@ -3,10 +3,34 @@ import React, { Component } from 'react'
 class Registry extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      link: '',
+      name: '',
+    }
   }
   render() {
-    return <div />
+    return (
+      <div>
+        <form>
+          <label htmlFor='link'>Registry Link</label>
+          <input
+            type='text'
+            name='link'
+            value={this.state.link}
+            placeholder='Add a Registry'
+            onChange={this.inputHandler}
+          />
+          <label htmlFor='name'>Display Name</label>
+          <input
+            type='text'
+            name='name'
+            value={this.state.name}
+            placeholder='Display Name'
+            onChange={this.inputHandler}
+          />
+        </form>
+      </div>
+    )
   }
 }
 
