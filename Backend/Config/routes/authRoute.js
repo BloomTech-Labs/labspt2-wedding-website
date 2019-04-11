9b82d05c46256bef7df6e019b53ed35bef26b082
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-04-10 19:14:04 -0500

    Backend: google redirect typo fix

--- Backend/Config/routes/authRoute.js
+++ Backend/Config/routes/authRoute.js
@@ -131,7 +131,7 @@ googleCB = (req, res) => {
   const token = jwtHelper.generateToken(tokenUser)
   // redirects to account set up
   console.log('token :', token)
-  res.redirect('https://joinourbigday.netlify.com/token=' + token)
+  res.redirect('https://joinourbigday.netlify.com/?token=' + token)
 }
 
 //------------------->Facebook Routes<--------------------------
