80917b742b95b15838c8ee30c6191b5faa14cafe
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-04-17 17:40:13 -0500

    FEregistry: created crud endpoints for registry route fixed typoi on question route created helpers for registry db

--- Backend/Config/routes/registryRoute.js
+++ Backend/Config/routes/registryRoute.js
@@ -1,9 +1,57 @@
-const helper = require('../helpers/questionsDB')
+const helper = require('../helpers/registryDb')
 const db = require('../dbConfig')
 
 module.exports = server => {
-  server.get('/registry/:id')
-  server.post('/registry/:id')
-  server.put('/registry/:id')
-  server.delete('/registry/:id/:registry')
+  server.get('/registry/:id', getUserRegistry)
+  server.post('/registry/:user', addNewRegistry)
+  server.put('/registry/:userId/:registryId', updateRegistry)
+  server.delete('/registry/:id/:registry', removeRegistry)
+}
+
+getUserRegistry = (req, res) => {
+  const { id } = req.params
+  helper
+    .getRegistryByUserId(id)
+    .then(registry => {
+      res.json(registry)
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'getting registry failed' })
+    })
+}
+
+addNewRegistry = (req, res) => {
+  const newR = req.body
+  const { user } = req.params
+  newR.userId = user
+  helper
+    .addRegistry(newR)
+    .then(id => {
+      res.status(201).json({ message: 'Registry added' })
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Adding registry failed' })
+    })
+}
+
+editRegistry = (req, res) => {
+  const registry = req.body
+  const { userId, registryId } = req.params
+  registry.userId = userId
+
+  helper
+    .updateRegistry(registryId, registry)
+    .then(() => {
+      res.status(202).json({ message: 'Registry updated' })
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Updating registry failed' })
+    })
+}
+
+removeRegistry = (req, res) => {
+  const { id } = req.params
+  helper.deleteRegistry(id).then(() => {
+    res.json({ message: 'Its gone!' })
+  })
 }
