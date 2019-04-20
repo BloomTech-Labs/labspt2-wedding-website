65d05a9a8e7b577b6f67d394a66909947d88965a
FreeKeyBoy <jjhutton81@gmail.com>
2019-04-20 15:19:07 -0700

    Built the customSite component. Moving on to debug and testing.

--- Frontend/client/src/components/design/customSite.js
+++ Frontend/client/src/components/design/customSite.js
@@ -5,6 +5,9 @@ import Axios from 'axios'
 import WeddingPage1 from './weddingPage1/WeddingPage1'
 import WeddingPage2 from './weddingPage2/WeddingPage2'
 import WeddingPage3 from './weddingPage3/WeddingPage3'
+import Spinner from './Spinner'
+
+const URL = "";
 
 class CustomSite extends Component {
   constructor(props) {
@@ -12,6 +15,11 @@ class CustomSite extends Component {
     this.state = {
       // everything needed for the site should be stored here
       loading: true,
+      userUrl: '',
+      story: '',
+      siteDesign: '',
+      proposalStory: '',
+      userId: ''
     }
   }
 
@@ -19,7 +27,8 @@ class CustomSite extends Component {
 
   //backend route still not tested, still need to seed data to test
 
-  /* here we make a call to the custom site route using the site url from the match.params.customSite it should return an array with 2 objects 
+  /* here we make a call to the custom site route using the site url from the match.params.customSite it should return an 
+  array with 2 objects 
 
     - first object should be from the custom site table for that site url (siteInfo)
     {
@@ -42,21 +51,50 @@ class CustomSite extends Component {
     */
 
   //we Should send all that to state and pass the state to the weddingSite depending on the design chosen by the user
-
+  componentDidMount() {
+    siteUrl = this.props.match.params.customSite
+    axios
+      .get('${URL}/customSite/:siteUrl')
+      console.log(siteUrl)
+      .then(response => {
+        console.log(response)
+          this.setState({
+            userUrl:userUrl
+          })
+      })
+      .catch(error => {
+        message: "There was a problem processing your request. Please try again."
+      })
+  }
   render() {
     /* 
       if statements for loading when loading is true show loading text/animation
 
-      if loading = false, return the weddingSite depending on the design chosen that should be on state (from custom site table) and pass state (nessesary info) through props
+      if loading = false, return the weddingSite depending on the design chosen that should be on state (from custom site table) 
+      and pass state (nessesary info) through props
 
       if userUrl is not found on the db.. return a not found type of page/message
     */
+
+  if (!userUrl) {
+    throw "Error 404: Page Not Found"
+  }
+
+  if (this.state.loading=true) {
+    return <Spinner /> 
+   } else {
     return (
       <div>
         <h1>{this.props.match.params.customSite}</h1>
       </div>
     )
   }
-}
 
 export default CustomSite
+
+/* I think the above needs to be written as 
+  export default function (props) {
+    return <h1>{props.match.params.customSite}</h1>
+  }
+  but I'm not positive
+  */
