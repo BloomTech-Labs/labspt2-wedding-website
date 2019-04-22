ddd8c3904934e197fe295946221b10e26f3a4891
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-04-22 17:05:07 -0500

    FSguestRsvp: deleted rsvp address table, since we are using google for the addres its saved in the correct format and the table is no longer needed added 2 columns to guestlist table created rsvp modal component to handle all the rsvp in it

---
+++ Frontend/client/src/components/modals/rsvp.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Rsvp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      guest: {
+        firstName: null,
+        lastName: null
+        
+      }
+    }
+  }
+  render() { 
+    return ();
+  }
+}
+
+export default Rsvp
