136bd771089e1e93c3efdb2f6deb39badeb4e833
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-04-30 22:37:31 -0500

    Frontend Styling: Moved StripeCheckout from billing page to pricing.

--- Frontend/client/src/components/billing/Billing.js
+++ Frontend/client/src/components/billing/Billing.js
@@ -1,53 +1,53 @@
-import React, { Component } from "react";
-import Button from "react-button-component";
-import SideNav from "../sidenav/sidenav";
+import React, { Component } from 'react'
+import Button from 'react-button-component'
+import SideNav from '../sidenav/sidenav'
 
 const button = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  width: "200px",
-  height: "100px",
-  boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
-  fontSize: "20px"
-};
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '200px',
+  height: '100px',
+  boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)',
+  fontSize: '20px',
+}
 
 const buttonDiv = {
-  display: "flex",
-  justifyContent: "center",
-  marginTop: "25px"
-};
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: '25px',
+}
 
 const billing = {
-  display: "flex",
-  flexDirection: "column",
-  margin: "0 auto",
-  width: "100%",
-  minWidth: "500px"
-};
+  display: 'flex',
+  flexDirection: 'column',
+  margin: '0 auto',
+  width: '100%',
+  minWidth: '500px',
+}
 
 const billBox = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  width: "300px",
-  height: "auto",
-  border: "1px solid #000000",
-  margin: "0 auto"
-};
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '300px',
+  height: 'auto',
+  border: '1px solid #000000',
+  margin: '0 auto',
+}
 
 const billInput = {
-  width: "90%",
-  height: "20px",
-  marginLeft: "auto",
-  marginRight: "auto",
-  marginTop: "10px",
-  marginBottom: "5px"
-};
+  width: '90%',
+  height: '20px',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  marginTop: '10px',
+  marginBottom: '5px',
+}
 
 const billHeader = {
-  textAlign: "center"
-};
+  textAlign: 'center',
+}
 
 export default class Billing extends Component {
   render() {
@@ -59,52 +59,51 @@ export default class Billing extends Component {
         <div style={billing}>
           <h2 style={billHeader}>Billing</h2>
           <div style={billBox}>
-            <label for="card-holder">Cardholder Name</label>
+            <label for='card-holder'>Cardholder Name</label>
             <input
               style={billInput}
-              type="text"
-              id="card-holder"
-              placeholder="Cardholder Name"
+              type='text'
+              id='card-holder'
+              placeholder='Cardholder Name'
             />
-            <label for="card-number">Card Number</label>
+            <label for='card-number'>Card Number</label>
             <input
               style={billInput}
-              type="text"
-              id="card-number"
-              placeholder="Card Number"
+              type='text'
+              id='card-number'
+              placeholder='Card Number'
             />
-            <label for="card-exp">Exp Date</label>
+            <label for='card-exp'>Exp Date</label>
             <input
               style={billInput}
-              type="text"
-              id="card-exp"
-              placeholder="Exp Date"
+              type='text'
+              id='card-exp'
+              placeholder='Exp Date'
             />
-            <label for="card-code">Special Code</label>
+            <label for='card-code'>Special Code</label>
             <input
               style={billInput}
-              type="text"
-              id="card-code"
-              placeholder="Special Code"
+              type='text'
+              id='card-code'
+              placeholder='Special Code'
             />
-            <label for="card-zip">Billing Zip</label>
+            <label for='card-zip'>Billing Zip</label>
             <input
               style={billInput}
-              type="text"
-              id="card-zip"
-              placeholder="Billing Zip"
+              type='text'
+              id='card-zip'
+              placeholder='Billing Zip'
             />
           </div>
           <div style={buttonDiv}>
             <Button
               style={button}
-              onClick={() => alert("Welcome to our site!")}
-            >
+              onClick={() => alert('Welcome to our site!')}>
               Buy Now
             </Button>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
--- Frontend/client/src/components/pricing/Pricing.js
+++ Frontend/client/src/components/pricing/Pricing.js
@@ -1,42 +1,39 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import './pricing.css'
-
+import StripeBtn from '../Stripe/stripeBtn'
 
 const HRStyle = styled.hr`
-    width: 50%;
-`;
-
+  width: 50%;
+`
 
 const PricingLogo = styled.h1`
   text-align: center;
 `
-
 
 const H2 = styled.h2`
   text-align: center;
 `
 
 export default class Pricing extends Component {
-    render() {
-        return (
-             <div className='pricePage'>
-             <div className='priceArea'>
-                 <PricingLogo>Pricing</PricingLogo>
-                 <HRStyle />
-                 <div className='boxStyle'>
-                    <ul>
-                        <li className='liStyle'>Planning Dashboard</li>
-                        <li className='liStyle'>Digital RSVPs</li>
-                        <li className='liStyle'>Free Website Hosting</li>
-                        <li className='liStyle'>Wedding Website</li>
-                    </ul>
-                 </div>
-                 <HRStyle />
-                 <H2>$0</H2>                 
-             </div>
-
-             </div>
-        );
-    }
+  render() {
+    return (
+      <div className='pricePage'>
+        <div className='priceArea'>
+          <PricingLogo>Go Premium</PricingLogo>
+          <HRStyle />
+          <div className='boxStyle'>
+            <ul>
+              <li className='liStyle'>Planning Dashboard</li>
+              <li className='liStyle'>Digital RSVPs</li>
+              <li className='liStyle'>Website Hosting</li>
+              <li className='liStyle'>Personal Wedding Website</li>
+            </ul>
+          </div>
+          <HRStyle />
+          <StripeBtn />
+        </div>
+      </div>
+    )
+  }
 }
