6fb96b036bc1380aad72c532be860ab3f1e13694
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-05-13 21:26:22 -0500

    localFrontend: added new address suggest to user setup . now it returns and link to google maps or guest. that can be clicked and sent to google maps page with the location of the venue

--- Frontend/client/src/components/landingPage/userSetup.js
+++ Frontend/client/src/components/landingPage/userSetup.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { editUser } from '../../actions'
 import { withRouter } from 'react-router'
-
+import VenueSearch from '../venueSearch'
 import styled from 'styled-components'
+import Script from 'react-load-script'
+import linkFunction from '../LinkFunction'
 
 import DatePicker from 'react-datepicker'
 import GoogleSuggest from '../googleSuggest'
+const InputWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2%;
+`
+
+const AddressInput = styled.input`
+  width: 80%;
+  padding: 0.5%;
+`
 
 const USBody = styled.div`
   display: flex;
@@ -81,12 +95,16 @@ class UserSetup extends Component {
       userInfo: {
         username: '',
         weddingParty: '',
+        //formatted_address
         venueLocation: '',
+        //return from link function
+        addressUrl: '',
         partnerName1: '',
         partnerName2: '',
         weddingDate: new Date(),
       },
       search: '',
+      query: '',
     }
   }
 
@@ -138,6 +156,46 @@ class UserSetup extends Component {
     })
   }
 
+  handleScriptLoad = () => {
+    // Initialize Google Autocomplete
+    /*global google*/ this.autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('autocomplete')
+    )
+
+    // Fire Event when a suggested name is selected
+    this.autocomplete.addListener('place_changed', this.handlePlaceSelect)
+  }
+
+  handlePlaceSelect = e => {
+    console.log(e)
+    // Extract City From Address Object
+    let addressObject = this.autocomplete.getPlace()
+    console.log('addres', addressObject)
+    let address = addressObject.address_components
+    const formattedAddress = addressObject.formatted_address
+    const lat = addressObject.geometry.location.lat()
+    const lng = addressObject.geometry.location.lng()
+    const place_id = addressObject.place_id
+
+    // Check if address is valid
+    if (address) {
+      // Set State
+      this.setState({
+        query: addressObject.name,
+        url: linkFunction(lat, lng, place_id),
+      })
+      this.setState({
+        // search: e.target.value, value: e.target.value
+        query: addressObject.name,
+        userInfo: {
+          ...this.state.userInfo,
+          venueLocation: formattedAddress,
+          addressUrl: linkFunction(lat, lng, place_id),
+        },
+      })
+    }
+  }
+
   handleSelectSuggest(suggest) {
     console.log(suggest)
     this.setState({
@@ -145,8 +203,10 @@ class UserSetup extends Component {
       userInfo: { ...this.state.userInfo, venueLocation: suggest },
     })
   }
+  handleInputChange = e => this.setState({ query: e.target.value })
 
   render() {
+    console.log(this.state.userInfo)
     return (
       <USBody>
         <FormWrapper>
@@ -194,12 +254,29 @@ class UserSetup extends Component {
               onChange={this.inputHandler}
             />
             <Label htmlFor=''>Venue Location</Label>
-            <GoogleSuggest 
+            {/* <GoogleSuggest 
               onChange={this.handleLocationChange.bind(this)}
               suggest={this.handleSelectSuggest.bind(this)}
               search={this.state.search}
               value={this.state.userInfo.venueLocation}
-            />
+            /> */}
+            <VenueSearch />
+            <InputWrap>
+              <Script
+                url='https://maps.googleapis.com/maps/api/js?key=AIzaSyAJfBhKctGiH0CzM2oygDTaRcqpkHxqzpw&libraries=places'
+                onLoad={this.handleScriptLoad}
+              />
+              <AddressInput
+                id='autocomplete'
+                placeholder='Enter Your Venue Address Here...'
+                value={this.state.query}
+                onChange={this.handleInputChange}
+                style={{
+                  margin: '0 auto',
+                  maxWidth: 800,
+                }}
+              />
+            </InputWrap>
             <Button onClick={this.handleSubmit}> Done </Button>
           </Form>
         </FormWrapper>
