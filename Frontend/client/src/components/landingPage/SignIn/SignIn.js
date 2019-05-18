69de310e220f5a2181cdc83c85f3dca33b8afc98
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-05-17 21:21:24 -0500

    frontend: fix

--- Frontend/client/src/components/landingPage/SignIn/SignIn.js
+++ Frontend/client/src/components/landingPage/SignIn/SignIn.js
@@ -101,7 +101,7 @@ class SignIn extends Component {
       if (!this.props.err) {
         this.props.history.push('/')
       }
-    }, 300)
+    }, 100)
 
     // need to implement nested routes so it can push to /dashboard, /profile, or something of that matter
   }
